30e7ead12fc0d1e8e7da8a5c58389caf4683750b
SamusDeCereza <20233l001130@utcv.edu.mx>
2025-02-01 04:14:32 -0600

    feat: Componentes de Ionic

---
+++ componentes/src/app/componentes/componentes.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componentes',
+  templateUrl: './componentes.page.html',
+  styleUrls: ['./componentes.page.scss'],
+  standalone: false
+})
+export class ComponentesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
